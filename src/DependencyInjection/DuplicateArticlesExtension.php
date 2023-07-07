b460b95c943d73c4227d36dc2264024f7e06c27f
Jeynova <novice.jeremy@gmail.com>
2023-07-07 14:48:48 +0200

    Add services yaml format

--- src/DependencyInjection/DuplicateArticlesExtension.php
+++ src/DependencyInjection/DuplicateArticlesExtension.php
@@ -9,11 +9,11 @@ use Symfony\Component\DependencyInjection\Loader;
 /**
  * SecurityHeadersExtension
  */
-class DuplicateContentCustomExtension extends Extension
+class DuplicateArticlesExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
     }
 }
