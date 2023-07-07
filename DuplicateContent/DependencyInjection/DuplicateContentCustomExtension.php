72f8d40e06cb29f0233f18ca632ba74399cc56ee
Jeynova <novice.jeremy@gmail.com>
2023-07-07 11:29:00 +0200

    Test modif architecture

--- DuplicateContent/DependencyInjection/DuplicateContentCustomExtension.php
+++ DuplicateContent/DependencyInjection/DuplicateContentCustomExtension.php
@@ -13,7 +13,7 @@ class DuplicateContentCustomExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/routes'));
         $loader->load('sulu_admin.yaml');
     }
 }
